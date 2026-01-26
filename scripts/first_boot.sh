4447f6fa7de54a3fd4ddfc51ae2d8db64707fdba
Alex Soares <alexsanderlimare@gmail.com>
2026-01-26 19:22:55 -0300

    Drop breeze-plus dep in illogical overlay if missing

--- scripts/first_boot.sh
+++ scripts/first_boot.sh
@@ -694,6 +694,8 @@ EOF
 						fi
 						# If songrec still not available, drop it to allow install
 						sed -i '/songrec/d' "$pkdir"/*.ebuild || true
+						# Drop breeze-plus dependency if missing in Gentoo tree
+						sed -i '/breeze-plus/d' "$pkdir"/*.ebuild || true
 						ebuild "$pkdir"/*.ebuild digest
 					done
 
